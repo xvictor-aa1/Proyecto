89be6b4483bbd4a757cb552fd539593e9dcea4c4
xvictor-aa1 <victormanuelfuenmayormn2@gmail.com>
2026-06-02 18:34:22 -0400

    Delete prueba-codigo directory

--- prueba-codigo/settings.py
+++
@@ -1,6 +0,0 @@
-AUTH_USER_MODEL = 'usuarios.Usuario'
-
-AUTHENTICATION_BACKENDS = [
-    'usuarios.auth_backend.CedulaOrUsernameBackend',
-    # 'django.contrib.auth.backends.ModelBackend',  # Opcional pero no necesario
-]
